53bc6fff5e8fb9a91dba7c9f65fb7ac7078f47ca
johalputt <ankushchoudharyjohal@gmail.com>
2026-06-24 14:13:06 +0000

    chore(release): bump version to 1.8.0

--- cmd/vayupress/main.go
+++ cmd/vayupress/main.go
@@ -68,7 +68,7 @@ import (
 	"github.com/johalputt/vayupress/internal/ws"
 )
 
-var Version = "1.7.0"
+var Version = "1.8.0"
 var bootTime = time.Now()
 
 // Immutable package-level values (compiled once, never mutated).
